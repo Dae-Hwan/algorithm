ce61cf1d5059b40c00a613fafad18bc1c4762628
Dae-Hwan <choidaehwan9282@gmail.com>
2022-02-10 22:17:03 +0900

    1021 백준

--- team_algorithm/1021_circle_que.py
+++ team_algorithm/1021_circle_que.py
@@ -1,31 +1,33 @@
+from collections import deque
 import sys
 
+# 뽑아내는데는 sprint 0 이동하면 sprint
 n, m = map(int, sys.stdin.readline().split())
 targets = list(map(int, sys.stdin.readline().split()))
-numbers = [i for i in range(1, n + 1)]
+numbers = deque([i for i in range(1, n + 1)])
 
 sprint = 0
 for i in range(m):
     target = targets[i]
     index = numbers.index(target)
 
-    if len(numbers) / 2 >= target:
+    if (len(numbers) / 2) >= index:
         while True:
             if target == numbers[0]:
-                del numbers[0]
+                numbers.popleft()
                 break
+
             else:
-                numbers.append(numbers[0])
-                del numbers[0]
+                numbers.rotate(-1)
                 sprint += 1
     else:
         while True:
             if target == numbers[0]:
-                del numbers[0]
+                numbers.popleft()
                 break
             else:
-                numbers.insert(0, numbers[-1])
-                del numbers[-1]
+                numbers.rotate(1)
                 sprint += 1
 
 print(sprint)
+
--- team_algorithm/4949.py
+++ team_algorithm/4949.py
@@ -9,11 +9,13 @@ while True:
         if s == '(' or s == '[':
             stack.append(s)
         elif s == ')':
+            # 길이가 0이면 이미 no 이기 때문
             if len(stack) != 0 and stack[-1] == '(':
                 stack.pop()
             else:
                 stack.append(')')
                 break
+
         elif s == ']':
             if len(stack) != 0 and stack[-1] == '[':
                 stack.pop()
