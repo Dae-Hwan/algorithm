567ac895a18f31176f0558225d40eedaee9b71c9
Dae-Hwan <choidaehwan9282@gmail.com>
2022-03-10 22:42:48 +0900

    11725 백준 - 트리의 부모 찾기(완성)

--- team_algorithm/11725-트리의 부모 찾기.py
+++ team_algorithm/11725-트리의 부모 찾기.py
@@ -13,13 +13,12 @@ def bfs(n):
         for i in graph[queue_n]:
             if not visited[i]:
                 queue.append(i)
-                visited[i] = True
+                visited[i] = queue_n
+
 
 N = int(sys.stdin.readline())
-
 graph = [[] for _ in range(N + 1)]
 visited = [False] * (N + 1)
-result = [[] for _ in range(N + 1)]
 
 # 중력이 생기기전
 for _ in range(N - 1):
@@ -27,6 +26,7 @@ for _ in range(N - 1):
     graph[a].append(b)
     graph[b].append(a)
 
-# for n in range(1, N + 1):
 bfs(1)
-print(result)
+for i in range(len(visited)):
+    if i >= 2:
+        print(visited[i])
