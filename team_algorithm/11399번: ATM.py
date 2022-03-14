a772a5986304866e85edd32e3a04ab62d8a8ea96
Dae-Hwan <choidaehwan9282@gmail.com>
2022-03-14 21:45:53 +0900

    11399 백준 - ATM 완성

--- team_algorithm/11399번: ATM.py
+++ team_algorithm/11399번: ATM.py
@@ -1,2 +1,18 @@
 import sys
-# dp
+
+N = int(sys.stdin.readline())
+numbers = list(map(int, sys.stdin.readline().split()))
+numbers.sort()
+
+sum = 0
+sum_list = []
+for number in numbers:
+    sum += number
+    sum_list.append(sum)
+
+
+result = 0
+for i in sum_list:
+    result += i
+
+print(result)
